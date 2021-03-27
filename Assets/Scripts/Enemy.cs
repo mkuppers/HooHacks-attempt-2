5070a37d15a743e21a09aa271024548db1655776
Mitchell Kuppersmith <mkuppers@umich.edu>
2021-03-27 13:47:16 -0400

    Basic setup

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Player user;
+    public bool aggro = true;
+    public float health = 100f;
+    public float exp = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        user = GameObject.Find("Player").GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Attacked
+    private void OnMouseDown()
+    {
+        if (aggro) {
+            takeDmg();
+        }
+    }
+
+    private void takeDmg() {
+        health -= user.strength;
+        if (health <= 0) {
+            die();
+        }
+    }
+
+    private void die() {
+        user.experience += exp;
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float strength = 10f;
+    public float experience = 0f;
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
